6fbe0cdba1c01ced384204c2e01558c0f54c17dc
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-05-11 22:46:03 -0400

    Ensure weird configuration isn't sent to the model

--- src/Event/Bake/TableEvent.php
+++ src/Event/Bake/TableEvent.php
@@ -10,22 +10,79 @@ class TableEvent extends Event
 {
     public function beforeRenderTable(CakeEvent $event)
     {
-        $subject = $event->subject;
-        $connection = ConnectionManager::get($subject->viewVars['connection']);
-        $schema = $connection->schemaCollection();
-
-        $tableSuffixes = ['revision', 'revisions', 'version', 'versions'];
+        $tableSuffixes = ['versions'];
         foreach ($tableSuffixes as $tableSuffix) {
-            $versionTable = sprintf('%s_%s', Hash::get($subject->viewVars, 'table'), $tableSuffix);
-
-            if (in_array($versionTable, $schema->listTables())) {
-                $event->subject->viewVars['behaviors']['Josegonzalez/Version.Version'] = [
-                    sprintf("'versionTable' => '%s'", $versionTable),
-                ];
+            if ($this->checkAssociation($event, $tableSuffix)) {
                 break;
             }
         }
 
+        $this->fixVersionTables($event);
+
         return $event;
+    }
+
+    protected function fixVersionTables(CakeEvent $event)
+    {
+        if (!preg_match('/Versions$/', $event->subject->viewVars['name'])) {
+            return;
+        }
+
+        unset($event->subject->viewVars['rulesChecker']['version_id']);
+        foreach ($event->subject->viewVars['associations']['belongsTo'] as $i => $association) {
+            if ($association['alias'] === 'Versions' && $association['foreignKey'] === 'version_id') {
+                unset($event->subject->viewVars['associations']['belongsTo'][$i]);
+            }
+        }
+    }
+
+    protected function checkAssociation(CakeEvent $event, $tableSuffix)
+    {
+        $subject = $event->subject;
+        $connection = ConnectionManager::get($subject->viewVars['connection']);
+        $schema = $connection->schemaCollection();
+
+        $versionTable = sprintf('%s_%s', Hash::get($event->subject->viewVars, 'table'), $tableSuffix);
+        if (!in_array($versionTable, $schema->listTables())) {
+            return false;
+        }
+
+        $event->subject->viewVars['behaviors']['Josegonzalez/Version.Version'] = [
+            sprintf("'versionTable' => '%s'", $versionTable),
+        ];
+
+        $event->subject->viewVars['associations']['belongsTo'] = $$this->modifyBelongsTo($event);
+        $event->subject->viewVars['rulesChecker'] = $this->modifyRulesChecker($event);
+
+        return true;
+    }
+
+    protected function modifyBelongsTo(CakeEvent $event)
+    {
+        $belongsTo = $event->subject->viewVars['associations']['belongsTo'];
+
+        foreach ($belongsTo as $i => $association) {
+            if ($association['alias'] !== 'Versions' || $association['foreignKey'] !== 'version_id') {
+                continue;
+            }
+
+            unset($belongsTo[$i]);
+        }
+        return $belongsTo;
+    }
+
+    protected function modifyRulesChecker(CakeEvent $event)
+    {
+        $rulesChecker = $event->subject->viewVars['rulesChecker'];
+
+        foreach ($rulesChecker as $key => $config) {
+            if (Hash::get($config, 'extra') !== 'Versions' || $key !== 'version_id') {
+                continue;
+            }
+
+            unset($rulesChecker[$key]);
+        }
+
+        return $rulesChecker;
     }
 }
